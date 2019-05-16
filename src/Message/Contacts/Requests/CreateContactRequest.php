2c363b9bab2b6c70494460ccf1492993fa2e0279
Max Yendall <yendallmax@gmail.com>
2019-05-16 16:41:00 +1000

    Further contact progress (addresses and phone number arrays)

--- src/Message/Contacts/Requests/CreateContactRequest.php
+++ src/Message/Contacts/Requests/CreateContactRequest.php
@@ -4,8 +4,39 @@ use PHPAccounting\XERO\Message\Customers\Responses\CreateContactResponse;
 
 class CreateContactRequest extends AbstractRequest
 {
+    /**
+     * @param $data
+     * @return array
+     */
+    public function getAddressData($data) {
+        $addresses = [];
+        foreach($data as $address) {
+            $newAddress = [];
+            $newAddress['AddressType'] = $address->address_type;
+            $newAddress['AddressLine1'] = $address->address_line_1;
+            $newAddress['City'] = $address->city;
+            $newAddress['PostalCode'] = $address->postal_code;
+            $newAddress['Country'] = $address->country;
+            array_push($addresses, $newAddress);
+        }
 
+        return $addresses;
+    }
 
+    /**
+     * @param $data
+     * @return array
+     */
+    public function getPhoneData($data) {
+        $phones = [];
+        foreach($data as $phone) {
+            $newPhone = [];
+            $newPhone['PhoneType'] = $phone->phone_type;
+            $newPhone['PhoneNumber'] = $phone->phone_number;
+        }
+
+        return $phones;
+    }
     /**
      * Get the raw data array for this message. The format of this varies from gateway to
      * gateway, but will usually be either an associative array, or a SimpleXMLElement.
@@ -22,15 +53,10 @@ class CreateContactRequest extends AbstractRequest
         $this->issetParam($data, 'FirstName', 'first_name');
         $this->issetParam($data, 'LastName', 'first_name');
         $this->issetParam($data, 'EmailAddress', 'email_address');
-        $this->issetParam($data, 'BankAccountDetails', 'bank_account_details');
-        $this->issetParam($data, 'TaxNumber', 'tax_number');
-        $this->issetParam($data, 'AccountsReceivableTaxType', 'accounts_receivable_tax_type');
-        $this->issetParam($data, 'AccountsPayableTaxType', 'accounts_payable_tax_type');
-        $this->issetParam($data, 'Addresses', 'addresses');
-        $this->issetParam($data, 'Phones', 'phones');
-        $this->issetParam($data, 'DefaultCurrency', 'default_currency');
-        $this->issetParam($data, 'XeroNetworkKey', 'xero_network_key');
-        $this->issetParam($data, 'ContactGroups', 'contact_groups');
+
+        $data['Phones'] = $this->getPhoneData($this->getParameter('phones'));
+        $data['Addresses'] = $this->getAddressData($this->getParameter('addresses'));
+
         if($this->getParameter('is_individual')) {
             $data['IsSupplier'] = false;
             $data['IsCustomer'] = true;
@@ -39,15 +65,6 @@ class CreateContactRequest extends AbstractRequest
             $data['IsSupplier'] = true;
             $data['IsCustomer'] = false;
         }
-//        $this->issetParam($data, 'SalesDefaultAccountCode', 'sales_default_account_code');
-//        $this->issetParam($data, 'PurchasesDefaultAccountCode', 'purchase_default_account_code');
-//        $this->issetParam($data, 'SalesTrackingCategories', 'sales_tracking_categories');
-//        $this->issetParam($data, 'PurchasesTrackingCategories', 'purchase_tracking_categories');
-//        $this->issetParam($data, 'TrackingCategoryName', 'tracking_category_name');
-//        $this->issetParam($data, 'TrackingCategoryOption', 'tracking_category_option');
-//        $this->issetParam($data, 'PaymentTerms', 'payment_terms');
-
-
 
         return $data;
     }
