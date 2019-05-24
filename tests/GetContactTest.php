f48233feda31321cc487363443a40ea84128cc46
Max Yendall <yendallmax@gmail.com>
2019-05-24 15:15:36 +1000

    Get Contact request and response

--- tests/GetContactTest.php
+++ tests/GetContactTest.php
@@ -27,17 +27,17 @@ class GetContactTest extends TestCase
                     'oauth' => [
                         'callback' => 'localhost',
                         'signature_method' => \XeroPHP\Remote\OAuth\Client::SIGNATURE_HMAC_SHA1,
-                        'consumer_key' => 'LEFVEZ26CAJQXOBLKNZGE5KDAY2HP3',
-                        'consumer_secret' => 'LIYZTFSOCIIZUWEYIQBVPHJS8VG39D',
+                        'consumer_key' => '',
+                        'consumer_secret' => '',
                         'signature_location' => \XeroPHP\Remote\OAuth\Client::SIGN_LOCATION_QUERY
                     ]
                 ]
             ];
             $gateway->setXeroConfig($config);
-            $gateway->setAccessToken('3XO4ECQIGEGLACV4UPPAXSTJ0UQ1AO');
-            $gateway->setAccessTokenSecret('7JTQX9IDKUQIEV3ODSPPQLEVHIG1Q0');
+            $gateway->setAccessToken('');
+            $gateway->setAccessTokenSecret('');
             $params = [
-                'accountingIDs' => ["50a40408-2e0b-4337-bbf8-de749be9fc9d"],
+                'accountingIDs' => [""],
                 'page' => 1
             ];
 
