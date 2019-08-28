c49a7f09a9121fb820cb6f5e694a818a95f88582
Max Yendall <yendallmax@gmail.com>
2019-08-28 17:40:51 +1000

    Inventory Item changes

--- tests/InventoryItems/CreateInventoryItemTest.php
+++ tests/InventoryItems/CreateInventoryItemTest.php
@@ -18,14 +18,21 @@ class CreateInventoryItemTest extends BaseTest
                 'is_buying' => true,
                 'description' => 'Development Operations',
                 'buying_description' => 'Development Operations',
+                'status' => 'ACTIVE',
+                'unit' => 'QTY',
+                'type' => 'Stock',
                 'purchase_details' => [
+                    'buying_account_id' => 55970294,
                     'buying_unit_price' => 100,
                     'buying_account_code' => 200,
+                    'buying_tax_type_id' => 10,
                     'buying_tax_type_code' => 'OUTPUT'
                 ],
                 'sales_details' => [
+                    'selling_account_id' => 55970288,
                     'selling_unit_price' => 150,
                     'selling_account_code' => 200,
+                    'selling_tax_type_id' => 10,
                     'selling_tax_type_code' => 'OUTPUT'
                 ]
             ];
